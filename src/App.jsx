8731976c529735cd98b2e1c059620ec09c445ae4
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-12 20:57:20 +0200

    added pie chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Calendar from "./components/calendar";
 import BarChart from "./components/BarChart";
+import PieChart from "./components/PieChart";
 
 import "./App.css";
 
@@ -9,8 +10,11 @@ function App() {
       <p className="text-center font-bold text-xl p-5">1. Calendar Lesson</p>
       <Calendar />
 
-      <p className="text-center font-bold text-xl p-5">1. Chart Lesson</p>
+      <p className="text-center font-bold text-xl p-5">2. Bar Chart Lesson</p>
       <BarChart />
+
+      <p className="text-center font-bold text-xl p-5">3. PIE Chart Lesson</p>
+      <PieChart />
 
     </div>
   );
---
+++ src/components/PieChartData.js
@@ -0,0 +1,32 @@
+export const PieChartData = [
+  {
+    id: "haskell",
+    label: "haskell",
+    value: 256,
+    color: "hsl(8, 70%, 50%)",
+  },
+  {
+    id: "python",
+    label: "python",
+    value: 448,
+    color: "hsl(216, 70%, 50%)",
+  },
+  {
+    id: "css",
+    label: "css",
+    value: 571,
+    color: "hsl(84, 70%, 50%)",
+  },
+  {
+    id: "c",
+    label: "c",
+    value: 271,
+    color: "hsl(31, 70%, 50%)",
+  },
+  {
+    id: "stylus",
+    label: "stylus",
+    value: 509,
+    color: "hsl(184, 70%, 50%)",
+  },
+];
