dfade8242cccdab0395c5d17dce47d33ed5a662f
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-12 18:55:06 +0200

    worked with bar chart setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Calendar from "./components/calendar";
+import BarChart from "./components/BarChart";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
+      <p className="text-center font-bold text-xl p-5">1. Calendar Lesson</p>
       <Calendar />
-    </>
+
+      <p className="text-center font-bold text-xl p-5">1. Chart Lesson</p>
+      <BarChart />
+
+    </div>
   );
 }
 
