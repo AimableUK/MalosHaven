cd598f95c449d5f12068da70e7015fff3a23b26e
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-19 10:40:14 +0200

    removed all theming things

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,17 @@ import AppLayout from "./components/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Team from "./pages/Team";
 import Invoices from "./pages/Invoices";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-
 import "./index.css";
 
 function App() {
-
   return (
-      <ThemeProvider>
-        <CssBaseline />
-
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="team" element={<Team />} />
-            <Route path="invoices" element={<Invoices />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="team" element={<Team />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
+    </Routes>
   );
 }
 
