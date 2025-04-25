7805c90579c736c4ddc01ebae98e28392574535a
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-25 09:43:47 +0200

    starting to work with the teants page

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import { Box, TextField } from '@mui/material'
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <Box>
+      <TextField 
+        
+      />
+    </Box>
+  )
+}
+
+export default SearchBar
--- src/pages/Booking&TenantManagement/TenantsPage.jsx
+++ src/pages/Booking&TenantManagement/TenantsPage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
 
 const TenantsPage = () => {
   return (
-    <div>
-      TenantsPage
-    </div>
-  )
-}
+    <Box m="20px">
+      <Box className="grid-cols-12 flex flex-row w-full">
+        <Box className="bg-[#2D454D] col-span-8">
+          <Typography fontWeight="bold">Tenants List</Typography>
+          <Box></Box>
+        </Box>
+        <Box className="col-span-4">
+          <Button>Add User</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default TenantsPage
+export default TenantsPage;
