5e06a3b7526b5553c458e8d8bd63cee9adb491c2
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-25 10:58:49 +0200

    working on the tenants page

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,40 @@
-import { Box, TextField } from '@mui/material'
-import React from 'react'
+import { Box, TextField, InputAdornment, IconButton } from "@mui/material";
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange, showClearIcon }) => {
+  const handleClick = () => {
+    onChange({ target: { value: "" } });
+  };
+
   return (
     <Box>
-      <TextField 
-        
+      <TextField
+        variant="outlined"
+        placeholder="Search..."
+        value={value}
+        onChange={onChange}
+        size="small"
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon sx={{color: "white"}}/>
+              </InputAdornment>
+            ),
+            endAdornment: showClearIcon === "flex" && (
+              <InputAdornment position="end">
+                <IconButton onClick={handleClick} edge="end" size="small">
+                  <ClearIcon sx={{color: "white"}}/>
+                </IconButton>
+              </InputAdornment>
+            ),
+          },
+        }}
       />
     </Box>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- src/pages/Booking&TenantManagement/TenantsPage.jsx
+++ src/pages/Booking&TenantManagement/TenantsPage.jsx
@@ -1,13 +1,56 @@
-import { Box, Button, Typography } from "@mui/material";
-import React from "react";
+import { Avatar, Box, Button, Divider, Typography } from "@mui/material";
+import React, { useState } from "react";
+import SearchBar from "../../components/SearchBar";
+import userAvatar from "../../assets/userAvatar.jpg";
+import tenants from "../../components/TenantsList";
 
 const TenantsPage = () => {
+  const [showClearIcon, setShowClearIcon] = useState("none");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    setShowClearIcon(event.target.value === "" ? "none" : "flex");
+  };
+
   return (
-    <Box m="20px">
-      <Box className="grid-cols-12 flex flex-row w-full">
-        <Box className="bg-[#2D454D] col-span-8">
-          <Typography fontWeight="bold">Tenants List</Typography>
-          <Box></Box>
+    <Box m="20px" display="flex" flexDirection="column">
+      <Box className="flex flex-col md:grid grid-cols-12">
+        <Box className="bg-[#2D454D] col-span-8 rounded-md p-5">
+          {/* tenants header */}
+          <Box className="flex flex-col md:flex-row justify-between">
+            <Typography fontWeight="bold">Tenants List</Typography>
+            <SearchBar
+              value={searchTerm}
+              onChange={handleChange}
+              showClearIcon={showClearIcon}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              height: "1px",
+              width: "100%",
+              background:
+                "linear-gradient(to right, #2d454d, #ABADAE, #2d454d)",
+              my: 1,
+              borderRadius: "999px",
+            }}
+          />
+          {/* Tenants */}
+          <Box className="flex flex-wrap">
+            {tenants.map((tenant) => (
+              <Box key={tenant.tenant_id} className="flex flex-col md:flex-row gap-3 border rounded p-3 m-1 shadow-sm w-[calc(30%-1rem)] min-w-[230px] max-w-[300px]">
+                <Avatar src={tenant.image} />
+                <Box className="flex flex-col">
+                  <Typography fontWeight="bold">{tenant.name}</Typography>
+                  <Typography sx={{ fontSize: "14px", color: "#D0D0D0" }}>
+                    {tenant.phone}
+                  </Typography>
+                </Box>
+              </Box>
+            ))}
+          </Box>
         </Box>
         <Box className="col-span-4">
           <Button>Add User</Button>
