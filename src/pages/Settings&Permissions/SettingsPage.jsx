09ad83d9174a6909ec5161b88141f840d45010c4
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-28 14:46:08 +0200

    changed the git repo and updated the app name and updated my settings page

--- src/pages/Settings&Permissions/SettingsPage.jsx
+++ src/pages/Settings&Permissions/SettingsPage.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 
 const SettingsPage = () => {
   return (
-    <div>
-      settingsPage
-    </div>
-  )
-}
+    <>
+      <Box m="20px" borderRadius="4px">
+        {/* breadCrumb */}
+        <Box sx={{background: "#24383E", p: 2}}>
+          <Typography>Settings/</Typography>
+        </Box>
+        {/* settings body */}
+        <Box className="bg-[#24383E] mt-3">
+          {/* boxes */}
+          <Box className="flex flex-row p-3">
+            <Person2OutlinedIcon sx={{fontSize: "50px"}}/>
+            <Box className="flex flex-col max-w-64">
+              <Typography fontWeight="bold">Profile Info</Typography>
+              <Typography color="#B9BCBD">
+                Password, Security, and Personal information
+              </Typography>
+            </Box>
+          </Box>
 
-export default SettingsPage
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default SettingsPage;
