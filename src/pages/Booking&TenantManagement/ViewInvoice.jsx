0f923ef56aecb0992ea6bc8087589b9b9541bb0d
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-05-22 20:24:27 +0200

    added some strick conditions to tenants for better payments

--- src/pages/Booking&TenantManagement/ViewInvoice.jsx
+++ src/pages/Booking&TenantManagement/ViewInvoice.jsx
@@ -35,69 +35,147 @@ const PrintableArticle = () => {
   return (
     <Box className="m-5">
       <Box
-        className="a4-page print:bg-white print:text-black p-6 bg-[#24383E] rounded-md"
+        className="flex flex-col gap-3 a4-page print:bg-white print:text-black p-6 bg-[#24383E] rounded-md"
         ref={componentRef}
       >
-        <Box className="flex flex-col gap-4">
-          <Box className="flex flex-row justify-between">
-            <img src={Logo} alt="Our Logo" />
-            <Typography fontFamily="poppins">
-              Invoice #<br /> {invoice.invoiceNumber}
-            </Typography>
-          </Box>
-          <Box className="flex flex-row justify-between">
-            <Box>
-              <Typography fontWeight="bold" fontFamily="poppins">
-                KAMO BUSINESS CO. LTD
-              </Typography>
-              <Typography variant="h7" fontWeight="bold">
-                Luxury Gatsata Center
-              </Typography>
-              <Typography fontFamily="poppins">Muhima, 120KN ST</Typography>
-              <Typography fontFamily="poppins">+250 780934382</Typography>
-            </Box>
-            <Box className="flex flex-col items-end">
-              <Typography fontFamily="poppins" fontWeight="bold">
-                {invoice.tenantName}
-              </Typography>
-              <Typography fontFamily="poppins">{invoice.email}</Typography>
-              <Typography fontFamily="poppins">{invoice.phone}</Typography>
-            </Box>
-          </Box>
-          <Box className="flex flex-col">
-            <Typography fontWeight="bold" fontFamily="poppins">
-              Issue Date:
-              <span style={{ fontWeight: "initial" }}>
-                {invoice.dateIssued}
-              </span>
-            </Typography>
-            <Typography fontWeight="bold" fontFamily="poppins">
-              Due Date:
-              <span style={{ fontWeight: "initial" }}>{invoice.dueDate}</span>
-            </Typography>
-          </Box>
+        <Box className="flex flex-row justify-between">
+          <img src={Logo} alt="Our Logo" />
+          <Typography fontFamily="poppins">
+            Invoice #<br /> {invoice.invoiceNumber}
+          </Typography>
+        </Box>
+        <Box className="flex flex-row justify-between">
           <Box>
-            <Box className="flex flex-row justify-between">
-              <Typography fontWeight="bold" fontStyle="poppins">Reason</Typography>
-              <Typography fontWeight="bold" fontStyle="poppins">Amount</Typography>
-            </Box>
-            <Box
-              sx={{
-                height: "2px",
-                width: "100%",
-                background:
-                  "linear-gradient(to right, #2d454d, white, #2d454d)",
-                my: 1,
-                borderRadius: "999px",
-              }}
-            />
-            <Box className="flex flex-row justify-between">
-              <Typography fontStyle="poppins">{invoice.reason}</Typography>
-              <Typography fontStyle="poppins">{invoice.amount}</Typography>
-            </Box>
+            <Typography fontWeight="bold" fontFamily="poppins">
+              KAMO BUSINESS CO. LTD
+            </Typography>
+            <Typography variant="h7" fontWeight="bold">
+              Luxury Gatsata Center
+            </Typography>
+            <Typography fontFamily="poppins">Muhima, 120KN ST</Typography>
+            <Typography fontFamily="poppins">+250 780934382</Typography>
+          </Box>
+          <Box className="flex flex-col items-end">
+            <Typography fontFamily="poppins" fontWeight="bold">
+              {invoice.tenantName}
+            </Typography>
+            <Typography fontFamily="poppins">{invoice.email}</Typography>
+            <Typography fontFamily="poppins">{invoice.phone}</Typography>
           </Box>
         </Box>
-        <Box></Box>
+        <Box className="flex flex-col">
+          <Typography fontWeight="bold" fontFamily="poppins">
+            Issue Date:
+            <span style={{ fontWeight: "initial" }}>{invoice.dateIssued}</span>
+          </Typography>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            Due Date:
+            <span style={{ fontWeight: "initial" }}>{invoice.dueDate}</span>
+          </Typography>
+        </Box>
+        <Box>
+          <Box className="flex flex-row justify-between">
+            <Typography fontWeight="bold" fontStyle="poppins">
+              Description
+            </Typography>
+            <Typography fontWeight="bold" fontStyle="poppins">
+              Amount (RWF)
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              height: "2px",
+              width: "100%",
+              background: "linear-gradient(to right, #2d454d, white, #2d454d)",
+              my: 1,
+              borderRadius: "999px",
+            }}
+          />
+          <Box className="flex flex-row justify-between">
+            <Typography fontStyle="poppins">Monthly Rent</Typography>
+            <Typography fontStyle="poppins">{invoice.amount}</Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            height: "2px",
+            width: "100%",
+            background: "linear-gradient(to right, #2d454d, white, #2d454d)",
+            my: 1,
+            borderRadius: "999px",
+          }}
+        />
+        <Box>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            Payment Status:&nbsp;
+            <span style={{ fontWeight: "initial" }}>{invoice.status}</span>
+          </Typography>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            Reason:&nbsp;
+            <span style={{ fontWeight: "initial" }}>{invoice.reason}</span>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            height: "2px",
+            width: "100%",
+            background: "linear-gradient(to right, #2d454d, white, #2d454d)",
+            my: 1,
+            borderRadius: "999px",
+          }}
+        />
+        <Box>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            PAYMENT INSTRUCTIONS:
+          </Typography>
+          <Typography component="p" fontFamily="poppins">
+            Please pay the above total to:
+          </Typography>
+          <Typography fontFamily="poppins">
+            Bank of Kigali - KAMO & SONS BUSINESS LTD
+          </Typography>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            Account Number:&nbsp;
+            <span style={{ fontWeight: "initial" }}>000123456789</span>
+          </Typography>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            Reference:&nbsp;
+            <span style={{ fontWeight: "initial" }}>
+              Invoice {invoice.invoiceNumber}
+            </span>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            height: "2px",
+            width: "100%",
+            background: "linear-gradient(to right, #2d454d, white, #2d454d)",
+            my: 1,
+            borderRadius: "999px",
+          }}
+        />
+        <Box>
+          <Typography fontWeight="bold" fontFamily="poppins">
+            NOTES:
+          </Typography>
+          <Box component="dl">
+            <Typography fontFamily="poppins">
+              Late Payment After {invoice.dueDate} may result in a 5% penalty
+            </Typography>
+            <Typography fontFamily="poppins">Contact us if you have made this payment</Typography>
+          </Box>
+        </Box>
+        <Typography fontFamily="poppins">THANK YOU FOR YOUR BUSINESS</Typography>
+        <Box className="flex flex-row justify-between">
+          <Box className="flex flex-col">
+            <Typography fontWeight="bold" fontFamily="poppins">KAMO 7 SONS B'SS LTD</Typography>
+            <Typography>____________________</Typography>
+          </Box>
+          <Box className="flex flex-col">
+            <Typography fontWeight="bold" fontFamily="poppins">{invoice.tenantName}</Typography>
+            <Typography>____________________</Typography>
+          </Box>
+        </Box>
       </Box>
 
       {/* Print Button */}
